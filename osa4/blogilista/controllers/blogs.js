6c5fcef971646bbe274b14d1a873c5302965f2b9
Salmi Sara M <salmisar@cs.helsinki.fi>
2020-10-01 14:13:55 +0300

    Fix the bad request error condition of exercise 4.12

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -9,7 +9,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  if(!body.title || !body.author) {
+  if(!body.title && !body.url) {
     return response.status(400).end()
   }
 
