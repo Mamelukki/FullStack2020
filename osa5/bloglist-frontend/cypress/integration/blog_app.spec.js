3567fc082063b4055f0b114009c1dc579cf020a9
Mamelukki <sara.m.salmi@gmail.com>
2020-11-13 14:45:20 +0200

    5.18: blogilistan end to end -testit, step2 + bonus exercise for checking the error message color

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -16,4 +16,26 @@ describe('Blog app', function() {
     cy.contains('password')
     cy.contains('login')
   })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error').should('contain', 'Wrong username or password') 
+      cy.get('.error').should('have.css', 'color', 'rgb(139, 0, 0)')
+      cy.get('.error').should('have.css', 'backgroundColor', 'rgb(240, 128, 128)')
+      cy.get('.error').should('have.css', 'border-style', 'solid')
+    })
+  })
 })
