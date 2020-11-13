9e2eabb648481f2a306f1f1e08cb7cba73709aa8
Mamelukki <sara.m.salmi@gmail.com>
2020-11-13 13:53:29 +0200

    5.17: blogilistan end to end -testit, step1

--- osa5/bloglist-backend/controllers/users.js
+++ osa5/bloglist-backend/controllers/users.js
@@ -1,33 +1,35 @@
-const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const User = require('../models/user')
 
+router.get('/', async (request, response) => {
+  const users = await User
+    .find({})
+    .populate('blogs', { title: 1, url: 1,  likes: 1, author: 1 })
+
+  response.json(users.map(u => u.toJSON()))
+})
+
 router.post('/', async (request, response) => {
-  const body = request.body
+  const { password, name, username } = request.body
 
-  const user = await User.findOne({ username: body.username })
-
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
-
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
+  if ( !password || password.length<3 ) {
+    return response.status(400).send({
+      error: 'password must min length 3'
     })
   }
 
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  }
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const user = new User({
+    username, name,
+    passwordHash,
+  })
 
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name })
+  const savedUser = await user.save()
+
+  response.json(savedUser)
 })
 
 module.exports = router
