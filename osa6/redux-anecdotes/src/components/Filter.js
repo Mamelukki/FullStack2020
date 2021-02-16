52e0c76692e7f91a051ccb5b8c7f3894de9ee7ff
Mamelukki <sara.m.salmi@gmail.com>
2021-02-16 19:23:29 +0200

    6.14 anekdootit ja backend, step2

--- osa6/redux-anecdotes/src/components/Filter.js
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -6,7 +6,6 @@ const Filter = () => {
   const dispatch = useDispatch()
 
   const handleChange = (event) => {
-    console.log(event.target.value)
     dispatch(setFilter(event.target.value))
     // input-kentän arvo muuttujassa event.target.value
   }
