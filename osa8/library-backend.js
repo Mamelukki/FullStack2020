3a1bb144375402219180b61f28a8fada878f63df
Mamelukki <sara.m.salmi@gmail.com>
2022-02-04 17:48:44 +0200

    8.6: Kirjan lisäys

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -93,7 +94,8 @@ const typeDefs = gql`
   }
 
   type Author {
-    name: String!
+    name: String!,
+    born: Int,
     bookCount: Int!
   }
 
@@ -103,6 +105,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!,
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }  
 `
 
 const resolvers = {
@@ -128,6 +139,17 @@ const resolvers = {
         const bookCount = books.filter(book => book.author === author.name).length
         return { ...author, bookCount }
       })
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find(author => author.name === book.author)) {
+        const newAuthor = { name: book.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+      return book
     }
   }
 }
