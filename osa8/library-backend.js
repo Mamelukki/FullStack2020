0d235fca8e19b87b56ed2d165bf9ff99e15143f2
Mamelukki <sara.m.salmi@gmail.com>
2022-02-04 15:30:47 +0200

    8.5: genren kirjat

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -100,7 +100,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!,
+    allBooks(author: String, genre: String): [Book!]!,
     allAuthors: [Author!]!
   }
 `
@@ -110,10 +110,18 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
       }
-      return books.filter(book => book.author === args.author)
+      if (args.author && args.genre) {
+        return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
+      }
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
+      }
     },
     allAuthors: () => {
       return authors.map(author => {
